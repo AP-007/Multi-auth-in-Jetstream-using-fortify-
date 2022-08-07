152cb41c459304d73f384023604182eac007d358
unknown <poudel0070@gmail.com>
2022-08-07 17:13:36 +0545

    multiauth project

---
+++ resources/views/student/lessons/index.blade.php
@@ -0,0 +1,13 @@
+<x-student-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Lessons List') }}
+        </h2>
+    </x-slot>
+
+    <div>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            Student accessible
+        </div>
+    </div>
+</x-student-layout>
